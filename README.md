00d534823f6998fa6f79b2527eb7b68b86945e9f
tanayanagekar <64320450+tanayanagekar@users.noreply.github.com>
2021-02-17 21:04:29 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# Twitter Social Graph
+Graphical Analysis of Twitter Socia Media Community using Gephi.
+
+### About Dataset
+Dataset has been fetched using Twitter API and Python Tweepy Module. 
+Recursive calls were made on API to get followers list starting with a base case of seed account.
+[Link](https://github.com/AasthaGupta/Twitter-Social-Graph/tree/master/Datasets)
+
+### Gephi
+- 3000 Nodes
+![Random Layout](http://i.imgur.com/CISeL8v.png)
+
+- 100 Nodes
+![Circular Layout](http://i.imgur.com/jlVkZSo.png)
+
+### Project
+This is a part of Acadmic Project Report under the course **Programming-IV** (COE-319) at [**NSIT**](www.nsit.ac.in).
+[Project Report](https://docs.google.com/document/d/1dnsRXbNLHV5tHxmQOi8e8WlNots3cR8M1lqsWqYDPmQ/edit?usp=sharing)
+
+### Contributors
+- Aastha Gupta (202CO14)
+- Aman Priyadarshi (216CO14)
---
+++ normalize.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Author: amaneureka
+# @Date:   2017-03-25 08:41:51
+# @Last Modified by:   Aastha Gupta
+# @Last Modified time: 2017-04-01 11:17:28
+
+import os
+import glob
+from sets import Set
+
+def create_node_and_edge_list(inputfiles):
+	nodes = Set()
+	edges = []
+	for filename in glob.glob(inputfiles):
+		node_a = os.path.splitext(os.path.basename(filename))[0]
+		nodes.add(node_a)
+		if len(nodes) > 99:
+			break
+		with open(filename, 'r') as file:
+			for line in file:
+				node_b = line[:-1]
+				nodes.add(node_b)
+				edges.append([node_a, node_b])
+	print 'dumping nodes...' + str(len(nodes))
+	with open('Datasets/nodes.txt', 'w') as file:
+		file.write('Id;Label\n')
+		id=1
+		for node in nodes:
+			file.write('{};{}\n'.format(id, node))
+			id=id+1
+	print 'dumping edges...' + str(len(edges))
+	with open('Datasets/edges.txt', 'w') as file:
+		file.write('Source;Target\n')
+		for edge in edges:
+			file.write('{};{}\n'.format(edge[0], edge[1]))
+
+create_node_and_edge_list("Datasets/*.dataset")
